af9bf046fa40e68411a689c91d39b9b7429071a3
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-11-19 22:26:20 -0500

    Se agrega script a EditarComuna

--- src/components/Comuna/EditarComuna.vue
+++ src/components/Comuna/EditarComuna.vue
@@ -39,3 +39,49 @@
     </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+export default {
+  name: 'EditarComuna',
+  data() {
+    return {
+      comuna: {
+        comu_codi: 0,
+        comu_nomb: '',
+        muni_codi: 0
+      },
+      municipios: []
+    }
+  },
+  methods: {
+    cancelar() {
+      this.$router.push({ name: 'Comunas' })
+    },
+    async updateComuna() {
+      const res = await axios.put(`http://127.0.0.1:8000/api/comunas/${this.comuna.comu_codi}`, this.comuna)
+      
+      if (res.status == 200) {
+        this.$router.push({ name: 'Comunas' })
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Comuna has been updated',
+          showConfirmButton: false,
+          timer: 2000
+        })
+      }
+    }
+  },
+  mounted() {
+    this.comuna.comu_codi = this.$route.params.id;
+    axios.get(`http://127.0.0.1:8000/api/comunas/${this.comuna.comu_codi}`)
+      .then(response => {
+        this.comuna = response.data.comuna;
+        this.municipios = response.data.municipios
+      })
+  }
+}
+</script>
