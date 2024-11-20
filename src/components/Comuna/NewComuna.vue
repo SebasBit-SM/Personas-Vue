903b8a228acfdbf34c83acb3a549b9a23268820b
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-11-19 22:37:59 -0500

    Se actualiza NewComuna

---
+++ src/components/Comuna/NewComuna.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="container text-start">
+      <h1 class="text-primary fw-bold">New</h1>
+      <div class="card">
+        <div class="card-header fw-bold">
+          Comuna
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="saveComuna">
+            <div class="row mb-3">
+              <label for="comu_codi" class="form-label">Código</label>
+              <div class="input-group">
+                <div class="input-group-text">
+                  <font-awesome-icon icon="tag" />
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="comu_codi"
+                  placeholder="Código comuna"
+                  disabled
+                  v-model="comuna.comu_codi"
+                />
+              </div>
+            </div>
+  
+            <div class="row mb-3">
+              <label for="comu_nomb" class="form-label">Nombre :</label>
+              <div class="input-group">
+                <div class="input-group-text">
+                  <font-awesome-icon icon="building" />
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="comu_nomb"
+                  placeholder="Nombre comuna"
+                  v-model="comuna.comu_nomb"
+                />
+              </div>
+            </div>
+  
+            <div class="row mb-3">
+              <label for="muni_codi" class="form-label">Municipio :</label>
+              <div class="input-group">
+                <div class="input-group-text">
+                  <font-awesome-icon icon="bank" />
+                </div>
+                <select
+                  class="form-select"
+                  v-model="muni_codi"
+                >
+                  <option selected value="0">Seleccione un municipio</option>
+                  <option
+                    v-for="municipio in municipios"
+                    :key="municipio.muni_codi"
+                    :value="municipio.muni_codi"
+                  >
+                    {{ municipio.muni_nomb }}
+                  </option>
+                </select>
+              </div>
+            </div>
+  
+            <button class="btn btn-primary" type="submit">Save</button>
+            <button class="btn btn-secondary mx-2" @click="cancel">Cancel</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </template>
